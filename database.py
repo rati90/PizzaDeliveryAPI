efe1d12815bbd4a86054cd09a4de551adaefb338
ratibakhtadze <ratibakhtadze@gmail.com>
2022-04-06 16:42:59 +0400

    first commit

---
+++ database.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+
+engine = create_engine('sqlite://///home/dev5/PycharmProjects/pizzaAPI/pizza_delivery.db',
+                       echo=True
+                       )
+
+
+Base=declarative_base()
+
+Session=sessionmaker()
+
