3498709a35672c32a839b069fc7842e6b8bc4dd9
ratibakhtadze <ratibakhtadze@gmail.com>
2022-04-07 15:27:03 +0400

    show all orders added

--- order_routes.py
+++ order_routes.py
@@ -1,13 +1,82 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, status
+from fastapi_jwt_auth import AuthJWT
+from fastapi.exceptions import HTTPException
+from models import User, Order
+from schemas import OrderModel
+from database import Session, engine
+from fastapi.encoders import jsonable_encoder
 
 order_router = APIRouter(
     prefix='/orders',
     tags=['orders']
 )
 
+session = Session(bind=engine)
+
 
 @order_router.get('/')
-async def hello():
-    return {'message':'hello World'}
+async def hello(Authorize: AuthJWT = Depends()):
+    try:
+        Authorize.jwt_required()
+    except Exception:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail="Invalid Token"
+                            )
+
+    return {'message': 'hello World'}
 
 
+@order_router.post('/order', status_code=status.HTTP_201_CREATED)
+async def place_an_order(order: OrderModel, Authorize: AuthJWT = Depends()):
+    try:
+        Authorize.jwt_required()
+    except Exception:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail="Invalid Token"
+                            )
+
+    current_user = Authorize.get_jwt_subject()
+
+    user = session.query(User).filter(User.username == current_user).first()
+
+    new_order = Order(
+        pizza_size=order.pizza_size,
+        quantity=order.quantity
+    )
+
+    new_order.user = user
+
+    session.add(new_order)
+    session.commit()
+
+    response = {
+        "pizza_size": new_order.pizza_size,
+        "quantity": new_order.quantity,
+        "id": new_order.id,
+        'order_status': new_order.order_status
+    }
+
+    return jsonable_encoder(response)
+
+
+@order_router.get('/orders')
+async def list_all_order(Authorize: AuthJWT = Depends()):
+    try:
+        Authorize.jwt_required()
+    except Exception:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail="Invalid Token"
+                            )
+
+    current_user = Authorize.get_jwt_subject()
+
+    user = session.query(User).filter(User.username == current_user).first()
+
+    if user.is_staff:
+        orders = session.query(Order).all()
+
+        return jsonable_encoder(orders)
+
+    raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                        detail="You are not a superuser"
+                        )
